ae30ce1fb85e013ba83561027cec20dbf52c38bc
Emnon <csampaie.5@gmail.com>
2021-05-06 23:11:58 +0200

    FINAL

--- hazi_2.js
+++ hazi_2.js
@@ -14,8 +14,6 @@ function vanENev(miben, ki) {
   }
   while (i < miben.length);
   console.log(ki + ' nem található a tömbben!');
-
-
 }
 
 vanENev(names, 'Gábor');
@@ -24,54 +22,80 @@ vanENev(names, 'Andrea');
 
 
 // 1. feladat
-// for ciklussal
+// probálgatások - az indexet adjuk vissza. Ha a sorszámát akarnánk akkor i+1
+function vanE(miben, ki) {
+  let benne_van = false;
+  for (let i = 0; i < miben.length; i++) {
+    if (miben[i] === ki) { benne_van = true; return i; }
+  }
+  return -1;
 
-// function vanENev_2(miben, ki) {
-//   for (let i = 0; i < miben.length; i++) {
-//     if (miben[i] === ki) {
-//       console.log(ki + ' a ' + parseInt(i + 1) + '. helyen található a tömbben!');
-//       return;
-//     }
-//     console.log(ki + ' nem található a tömbben!');
-//   }
-// }
-
-// vanENev_2(names, 'Gábor');
-// vanENev_2(names, 'István');
-// vanENev_2(names, 'Andrea');
+}
+let van = vanE(names, 'Attila');
+console.log(van);
 
 
+// 1. feladat
+// probálgatások 2 - az indexet adjuk vissza. Ha a sorszámát akarnánk akkor i+1
+function vanE_2(miben, ki) {
+
+  for (let i = 0; i < miben.length; i++) {
+    if (miben[i] === ki) {
+      console.log(ki + ' benne van a tömben a(z) ' + i + '. indexen');
+      return;
+    }
+  }
+  return console.log(ki + ' nincs benne van a tömben!');
+}
+van = vanE_2(names, 'Andrea');
 
 // 2. feladat
 
-function sum(tomb) {
-  let result = 0;
-  for (let i = 0; i < tomb.length; i++) {
-    result += tomb[i];
-  }
-  return result;
-}
+// function sum(tomb) {
+//   let result = 0;
+//   for (let i = 0; i < tomb.length; i++) {
+//     result += tomb[i];
+//   }
+//   return result;
+// }
 
-const nums = [4, 2, 2, 1, 1];
-let sum_var;
-console.log('Az eredmény: ' + (sum_var = sum(nums)));
+// const nums = [4, 2, 2, 1, 1];
+// let sum_var;
+// console.log('Az eredmény: ' + (sum_var = sum(nums)));
 
 
 // 3. feladat
 
-function sum(tomb) {
-  let result = 0;
-  for (let i = 0; i < tomb.length; i++) {
-    if (typeof (tomb[i]) !== 'number') {
-      console.log('Az alábbi elem: ' + '-' + tomb[i] + '-' + ' nem szám!');
-    } else {
-      result += tomb[i];
-    }
+// function sum(tomb) {
+//   let result = 0;
+//   for (let i = 0; i < tomb.length; i++) {
+//     if (typeof (tomb[i]) !== 'number') {
+//       console.log('Az alábbi elem: ' + '-' + tomb[i] + '-' + ' nem szám!');
+//     } else {
+//       result += tomb[i];
+//     }
+//   }
+//   return result;
+// }
 
-  }
-  return result;
-}
+// const nums_2 = ['a', true, '2', '2', '1', 1];
+// let sum_var_2;
+// console.log('Az eredmény: ' + (sum_var_2 = sum(nums_2)));
 
-const nums_2 = ['a', true, 2, 2, 1, 1];
-let sum_var_2;
-console.log('Az eredmény: ' + (sum_var_2 = sum(nums_2)));
+// 4.feladat
+
+// function sum_advance(tomb) {
+//   let result = 0;
+//   for (let i = 0; i < tomb.length; i++) {
+//     if (typeof (tomb[i]) !== 'number') {
+//       if (isNaN(parseInt(tomb[i]))) {
+//         console.log('Az alábbi elem: ' + '[' + tomb[i] + ']' + ' nem szám!');
+//         continue;
+//       }
+//     }
+//     result += parseInt(tomb[i]);
+//   }
+//   return result;
+
+// }
+// console.log('Az eredmény: ' + (sum_var_2 = sum_advance(nums_2)));
