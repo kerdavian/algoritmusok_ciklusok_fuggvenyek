a0363a4cbc30374b210e74139bbf758d26763abf
Emnon <csampaie.5@gmail.com>
2021-05-15 09:32:44 +0200

    kommentekkel

--- hazi_1.js
+++ hazi_1.js
@@ -1,18 +1,19 @@
 // 1. feladat
-const nums = [3, 4, 9, 6, 2];
+const nums = [3, 4, 9, 6, 2]; //tömd deklarációés értékadás
 
 for (let i = 0; i < nums.length; i++) {
-  if (nums[i] % 2 === 0) {
-    console.log(nums[i] + ':' + 'osztható');
+  if (nums[i] % 2 === 0) { //osztható-e kettővel
+    console.log(nums[i] + ':' + 'osztható'); // ha igen
   } else {
-    console.log(nums[i] + ':' + 'nem osztható');
+    console.log(nums[i] + ':' + 'nem osztható'); // ha nem
   }
 }
 
 // 2. feladat
-const player = ['Péter', 'Kate', 'John'];
-for (let i = 0; i < player.length; i++) {
-  // ez írja ki az indexet
+const player = ['Péter', 'Kate', 'John']; //tömd deklarációés értékadás
+for (let i = 0; i < player.length; i++) { // végig itelárunk a tömbön
+
+  // ez írjné ki ki az indexet
   // console.log(i + '. ' + player[i]);
 
   // A példa nem az idexet írjaki, hanem a sorszámot
@@ -20,16 +21,16 @@ for (let i = 0; i < player.length; i++) {
 }
 
 // 3.feladat
-function tipusVizsgalo(tomb) {
-  let tipus_tomb = [];
-  for (let i = 0; i < tomb.length; i++) {
-    tipus_tomb.push(typeof (tomb[i]));
+function tipusVizsgalo(tomb) { //függvény deklaráció
+  let tipus_tomb = []; // tömb deklaráció
+  for (let i = 0; i < tomb.length; i++) { // végig itelárunk a tömbön
+    tipus_tomb.push(typeof (tomb[i])); // a tipus_tomb-hoz hozzáfűzzük a paraméterként kapot tomb változó i. eleménektípusát
   }
 
-  return tipus_tomb;
+  return tipus_tomb; // visszatérünk a tipus_tömbbel, ami tartalmazza a paraméterként kapott tömb változó elemeit
 }
-const x = ['', 4, true];
-let eredmeny_tomb = tipusVizsgalo(x);
+const elemek = ['', 4, true]; // deklarálunk egy tömböt tés adunk neki értéket
+let eredmeny_tomb = tipusVizsgalo(elemek); // meghívjuk a típusVizgálófüggvény az elemek tömb-bel és azt az értéket amivel a függvény visszatér értéküladjük az eredemny_tombváltozonak.
 
-console.log(eredmeny_tomb);
+console.log(eredmeny_tomb); //kilogoljuk az eredmény_tomb értékét
 
--- hazi_2.js
+++ hazi_2.js
@@ -1,3 +1,4 @@
+// az evidens dolgokat már nem kommentelem!!!
 // 1. feladat
 // while ciklusaal
 
@@ -5,15 +6,17 @@ const names = ['István', 'Katalin', 'Attila', 'Gábor', 'Péter', 'Anna', 'Kitt
 
 function vanENev(miben, ki) {
   let i = 0;
+  // do while ciklus
   do {
-    if (miben[i] === ki) {
-      console.log(ki + ' a ' + parseInt(i + 1) + '. helyen található a tömbben!');
-      return;
+    // abelsejében levizsgálunk valami eseményt
+    if (miben[i] === ki) { //ha benne van
+      console.log(ki + ' a ' + parseInt(i + 1) + '. helyen található a tömbben!'); //kiirjuk, hogy benne van és hogy hol
+      return; // kiléünk a ciklusból
     }
-    i++;
+    i++; // növelni kell az i változóértékét, hogy ne kerüljünk végtelen ciklusba
   }
-  while (i < miben.length);
-  console.log(ki + ' nem található a tömbben!');
+  while (i < miben.length); // addig megyünk,amilyen hosszú a paraméterként kapott miben tömb
+  console.log(ki + ' nem található a tömbben!'); // kilogoljuk, hogy a ki mrgtalálható-e a tömbben
 }
 
 vanENev(names, 'Gábor');
@@ -22,14 +25,17 @@ vanENev(names, 'Andrea');
 
 
 // 1. feladat
+// for ciklussal
 // probálgatások - az indexet adjuk vissza. Ha a sorszámát akarnánk akkor i+1
 function vanE(miben, ki) {
-  let benne_van = false;
-  for (let i = 0; i < miben.length; i++) {
-    if (miben[i] === ki) { benne_van = true; return i; }
+  //let benne_van = false; // deklaráunk egy noolean tipusu változót,aminek alaéprtelmezetten a false értéket adjuk
+  for (let i = 0; i < miben.length; i++) { // végig megyünk a miben paraméter tömbön
+    if (miben[i] === ki) { return i; } // ha benn van, akkor vissztérünk az i-vel benne_van változó =true benne_van = true;
   }
-  return -1;
+  return -1; // ha nincs benne akkor minus eggyel
 }
+
+// ez a változat szerintem programozósabb, úgyanis a függvény azt mondja meg nekünk hogy a keresett néz hányadik helyen van. Ezután mi eldönthetjük mit akarunk kezdeni ezzel az infóval
 let nev = 'Attila';
 let van = vanE(names, nev);
 if (van > 0) {
@@ -41,6 +47,7 @@ if (van > 0) {
 
 // 1. feladat
 // probálgatások 2 - az indexet adjuk vissza. Ha a sorszámát akarnánk akkor i+1
+// ez gyakorlatilag az előző feladat csak rögtön logoljuk az eredményt
 function vanE_2(miben, ki) {
 
   for (let i = 0; i < miben.length; i++) {
@@ -53,14 +60,14 @@ function vanE_2(miben, ki) {
 }
 van = vanE_2(names, 'Andrea');
 
-// 2. feladat
 
+// 2. feladat
 function sum(tomb) {
-  let result = 0;
+  let result = 0; // kell egy változó ami az összeget fogja tárolni
   for (let i = 0; i < tomb.length; i++) {
-    result += tomb[i];
+    result += tomb[i]; // mindig növeljül a result értékét a tomb paraméter i. elemének értékével.
   }
-  return result;
+  return result; // vissztérünk az eredménnyel
 }
 
 const nums = [4, 2, 2, 1, 1];
@@ -73,29 +80,31 @@ console.log('Az eredmény: ' + (sum_var = sum(nums)));
 function sum(tomb) {
   let result = 0;
   for (let i = 0; i < tomb.length; i++) {
-    if (typeof (tomb[i]) !== 'number') {
-      console.log('Az alábbi elem: ' + '-' + tomb[i] + '-' + ' nem szám!');
+    if (typeof (tomb[i]) !== 'number') { // levizsgáljuk, hogy a tömb paraméter i. elemé
     } else {
-      result += tomb[i];
+      result += tomb[i]; // ha igen
     }
   }
-  return result;
+  return result; // visszatérünk az eredénnyel, de csak a number tipusokat adtuk össze
 }
 
 const nums_2 = ['a', true, '2', '2', '1', 1];
 let sum_var_2;
 console.log('Az eredmény: ' + (sum_var_2 = sum(nums_2)));
 
+
+
 // 4.feladat
 
 function sum_advance(tomb) {
   let result = 0;
   for (let i = 0; i < tomb.length; i++) {
-    if ((typeof (tomb[i]) !== 'number') && (isNaN(parseInt(tomb[i])))) {
-      console.log('Az alábbi elem: ' + '[' + tomb[i] + ']' + ' nem szám!');
-      continue;
+    if ((typeof (tomb[i]) !== 'number') && (isNaN(parseInt(tomb[i])))) { // ha tomb paraméter i. elemének a típusa nem number és átalakítva sem number lesz akkor
+      console.log('Az alábbi elem: ' + '[' + tomb[i] + ']' + ' nem szám!'); // nem számolunk vele
+      continue; // következeő iteráció
     }
-    result += parseInt(tomb[i]);
+    //ha tomb paraméter i. elemének a típusa nem number édes átalakítva number lesz akkor
+    result += parseInt(tomb[i]); // az adot elem értékével növeljük a result értékét
   }
   return result;
 
